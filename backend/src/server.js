f838568c1d87bf7cc2f1aaae5e8131df8f6eaf20
mayur shelke <shelkem402@gmail.com>
2025-01-21 20:40:10 +0530

    made changes cart and uploade image

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';  // Use ES Module import for the router
 import productrouter from './routes/productRoute.js';
 import cartrouter from './routes/cartRouter.js';
+import uploadrouter from './routes/uploadRoutes.js';
 
 dotenv.config();  // Load environment variables
 
@@ -15,8 +16,13 @@ const port = 4000;
 app.use(express.json());  // Parse incoming requests with JSON payload
 app.use(cors());          // Enable CORS for the frontend (localhost:5173 in your case)
 
+app.use(express.urlencoded({ extended: true }));
+
+
+
 // Connect to the database
 connectDB();
+
 
 // Use the userRouter for authentication and user-related routes
 app.use("/api/user", userRouter);
@@ -24,6 +30,10 @@ app.use("/api/user", userRouter);
 app.use("/api/product", productrouter);
 
 app.use("/api/cart", cartrouter);
+
+app.use('/api/img', uploadrouter);
+
+
 // Basic route for testing
 app.get('/', (req, res) => {
     console.log("hello world!");
